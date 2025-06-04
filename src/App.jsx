69b196a1194ab1dad1467cd7a29402c4780a484f
RathodMohit-94 <mohitrathod9328@gmail.com>
2025-06-04 16:36:45 +0530

    elements of this

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import About from './components/About';
+import Resume from './components/Resume';
+import Contact from './components/Contact'
+
 // const inputValue = prompt("Enter your name:");
 // let name = 'Mohit';
 const loggedin = true;
 const App = () => {
   return (
     <>
-    <h1 className=' font-serif bg-emerald-300 flex justify-center items-center w-full h-52 text-9xl
-    '>Hello  &nbsp; { loggedin ? <h1>Member</h1> : <h1>Guest</h1>}</h1>
+    <Navbar/>
+    <Hero title="Unleash your vision with innovative front-end solutions tailored for the future."  header="Crafting Digital Experiences with Precision"/>
+    <About  aboutself='I`m Frontend Developer With Skills like HTML5 , CSS, JS, ReactJS , And TailWindCSS. ' writeAbout='' birthday='23/01/2006' websiteLink='' phoneNum='9328746209' cityState='Ahmedabad Gujarat' age='19' degree='Persuing BSc(AI&DS)' email='mohitrathod9328@gmail.com' classification='Front-End Developer' />
+    <Resume />
+    <Contact  phonenumContact='9328746209' emailContact='23010303020@knu.edu.in'/>
     </>
   )
 }
